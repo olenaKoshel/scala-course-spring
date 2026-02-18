34f1c0a28a0e187c97abac26ca78c22cd6786cf3
okoshel <okoshel@kse.org.ua>
2026-02-18 15:03:00 +0200

    optional task

--- src/main/scala/kse/unit1/challenge/arithmetic.scala
+++ src/main/scala/kse/unit1/challenge/arithmetic.scala
@@ -10,30 +10,41 @@ object arithmetic:
 
   val S: Number => Number = (x: Number) => x + 1
 
-  /**
-   * Optional task: make `addition` tail-recursive.
-   */
   val addition: (Number, Number) => Number =
     (m, n) =>
       require(n >= 0 && m >= 0, "should be positive or 0")
-      (m, n) match
-        case (m, 0) => m
-        case (m, n) => S(addition(m, n-1))
 
-  /**
-   * Optional task: make `multiplication` tail-recursive.
-   */
+      @tailrec
+      def additionRec(acc: Number, n: Number): Number =
+        n match
+          case 0 => acc
+          case _ => additionRec(S(acc), n-1L)
+      additionRec(m,n)
+
+
   val multiplication: (Number, Number) => Number =
     (m, n) =>
       require(n >= 0 && m >= 0, "should be positive or 0")
-      (m,n) match
-        case (m, 0) => 0
-        case (m,n) => addition(multiplication(m,n-1), m)
+
+      @tailrec
+      def multRec(acc: Number, n: Number): Number =
+        n match
+          case 0 => 0
+          case 1 => acc
+          case _ => multRec(addition(acc, m), n-1L)
+
+      multRec(m,n)
 
 
   val power: (Number, Number) => Number =
-    (m,n) =>
-      require(n >= 0 && m >= 0, "should be positive or 0")
-      (m,n) match
-        case (m,0) => 1
-        case (m,n) => multiplication(power(m,n-1),m)
+    (b,p) =>
+      require(p >= 0 && b >= 0, "should be positive or 0")
+
+      @tailrec
+      def powRec(acc: Number, n: Number): Number =
+        n match
+          case 0L => 1L
+          case 1L => acc
+          case n => powRec(multiplication(acc, b), n-1L)
+
+      powRec(b,p)
