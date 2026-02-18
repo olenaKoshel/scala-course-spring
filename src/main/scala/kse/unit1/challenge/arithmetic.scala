41e35b6a25832096692dd3293694da685dfa0bbf
okoshel <okoshel@kse.org.ua>
2026-02-18 14:32:26 +0200

    no optional task

--- src/main/scala/kse/unit1/challenge/arithmetic.scala
+++ src/main/scala/kse/unit1/challenge/arithmetic.scala
@@ -6,20 +6,36 @@ object arithmetic:
 
   type Number = Long
 
-  val Z: Number => Number = ???
-  val S: Number => Number = ???
+  val Z: Number => Number = (x: Number) => 0
+
+  val S: Number => Number = (x: Number) => x + 1
 
   /**
    * Optional task: make `addition` tail-recursive.
    */
-  val addition: (Number, Number) => Number = ???
+  val addition: (Number, Number) => Number =
+    (m, n) =>
+      require(n >= 0 && m >= 0, "should be positive or 0")
+      (m, n) match
+        case (m, 0) => m
+        case (m, n) => S(addition(m, n-1))
 
   /**
    * Optional task: make `multiplication` tail-recursive.
    */
-  val multiplication: (Number, Number) => Number = ???
+  val multiplication: (Number, Number) => Number =
+    (m, n) =>
+      require(n >= 0 && m >= 0, "should be positive or 0")
+      (m,n) match
+        case (m, 0) => 0
+        case (m,n) => addition(multiplication(m,n-1), m)
 
   /**
    * Optional task: make `power` tail-recursive.
    */
-  val power: (Number, Number) => Number = ???
+  val power: (Number, Number) => Number =
+    (m,n) =>
+      require(n >= 0 && m >= 0, "should be positive or 0")
+      (m,n) match
+        case (m,0) => 1
+        case (m,n) => multiplication(power(m,n-1),m)
