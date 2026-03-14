1563eb26436c8d0e56e6ef78bfeaa4db3ce4ecc7
okoshel <okoshel@kse.org.ua>
2026-03-14 12:18:50 +0100

    fix

--- src/test/scala/kse/unit2/challenge/BooleansSpecification.scala
+++ src/test/scala/kse/unit2/challenge/BooleansSpecification.scala
@@ -21,49 +21,57 @@ end BooleansSpecification
 object NegationSpecification extends Properties("Negation"):
 
   property("!True is False") = propBoolean:
-    ???
+    !True == False
 
   property("!False is True") = propBoolean:
-    ???
+    !False == True
 
 end NegationSpecification
 
 object ConjunctionSpecification extends Properties("Conjunction"):
 
   property("True ∧ value is value") = forAll: (value: Boolean) =>
-    ???
+    (True ∧ value) == value
 
   property("False ∧ value is False") = forAll: (value: Boolean) =>
-    ???
+    (False ∧ value) == False
+
 
   property("False ∧ value should ignore the second argument") = forAll: (value: Boolean) =>
-    ???
+    var evaluated = false
+    val result = False ∧ { evaluated = true; value }
+    !evaluated
 
 end ConjunctionSpecification
 
 object DisjunctionSpecification extends Properties("Disjunction"):
 
   property("True ∨ value is True") = forAll: (value: Boolean) =>
-    ???
+    (True ∨ value) == True
 
   property("True ∨ value should ignore the second argument") = forAll: (value: Boolean) =>
-    ???
+    var evaluated = false
+    val result = True ∨ { evaluated = true; value }
+    !evaluated
+
 
   property("False ∨ value is value") = forAll: (value: Boolean) =>
-    ???
+    (False ∨ value) == value
 
 end DisjunctionSpecification
 
 object ImplicationSpecification extends Properties("Implication"):
 
   property("True → value is value") = forAll: (value: Boolean) =>
-    ???
+    (True → value) == value
 
   property("False → value is True") = forAll: (value: Boolean) =>
-    ???
+    (False → value) == True
 
   property("False → value should ignore the second argument") = forAll: (value: Boolean) =>
-    ???
+    var evaluated = false
+    val result = False → { evaluated = true; value }
+    !evaluated
 
 end ImplicationSpecification
 
@@ -74,27 +82,38 @@ end EquivalenceSpecification
 
 object AxiomsSpecification extends Properties("Axioms"):
 
-  property("a → (b → a)") = ???
+  property("a → (b → a)") = forAll: (a: Boolean, b: Boolean) =>
+    (a → (b → a)) == True
 
-  property("(a → (b → c)) → ((a → b) → (a → c))") = ???
+  property("(a → (b → c)) → ((a → b) → (a → c))") = forAll: (a: Boolean, b: Boolean, c: Boolean) =>
+    ((a → (b → c)) → ((a → b) → (a → c))) == True
 
-  property("(a ∧ b) → a") = ???
+  property("(a ∧ b) → a") = forAll: (a: Boolean, b: Boolean) =>
+    ((a ∧ b) → a) == True
 
-  property("(a ∧ b) → b") = ???
+  property("(a ∧ b) → b") = forAll: (a: Boolean, b: Boolean) =>
+    ((a ∧ b) → b) == True
 
-  property("a → (b → (a ∧ b))") = ???
+  property("a → (b → (a ∧ b))") = forAll: (a: Boolean, b: Boolean) =>
+    (a → (b → (a ∧ b))) == True
 
-  property("a → (a ∨ b)") = ???
+  property("a → (a ∨ b)") = forAll: (a: Boolean, b: Boolean) =>
+    (a → (a ∨ b)) == True
 
-  property("b → (a ∨ b)") = ???
+  property("b → (a ∨ b)") = forAll: (a: Boolean, b: Boolean) =>
+    (b → (a ∨ b)) == True
 
-  property("(a → c) → ((b → c) → ((a ∨ b) → c))") = ???
+  property("(a → c) → ((b → c) → ((a ∨ b) → c))") = forAll: (a: Boolean, b: Boolean, c: Boolean) =>
+    ((a → c) → ((b → c) → ((a ∨ b) → c))) == True
 
-  property("!a → (a → b)") = ???
+  property("!a → (a → b)") = forAll: (a: Boolean, b: Boolean) =>
+    ((!a) → (a → b)) == True
 
-  property("(a → b) → ((a → !b) → !a)") = ???
+  property("(a → b) → ((a → !b) → !a)") = forAll: (a: Boolean, b: Boolean) =>
+    ((a → b) → ((a → (!b)) → (!a))) == True
 
-  property("a ∨ !a") = ???
+  property("a ∨ !a") = forAll: (a: Boolean) =>
+    (a ∨ (!a)) == True
 
 end AxiomsSpecification
 
